9793eb56f5c3999d143cb09ea62a75367fa2fdf4
James Morrison <jamesmorrison3@live.co.uk>
2021-03-26 15:38:34 +0000

    Remove dead code

--- ciphers.go
+++ ciphers.go
@@ -42,9 +42,14 @@ func NewUnauthenticatedGCM(isEncrypter bool, key []byte, iv []byte) (cipher.Bloc
 	ccipher := C.EVP_aes_256_gcm()
 	cipher := &Cipher{ptr: ccipher}
 
-	/* Initialise the encryption operation. */
-	if 1 != C.EVP_DecryptInit_ex(ctx.ctx, cipher.ptr, nil, keyptr, ivptr) {
-		return nil, errors.New("Error decrypt init")
+	if isEncrypter {
+		if 1 != C.EVP_EncryptInit_ex(ctx.ctx, cipher.ptr, nil, keyptr, ivptr) {
+			return nil, errors.New("Error encrypt init")
+		}
+	} else {
+		if 1 != C.EVP_DecryptInit_ex(ctx.ctx, cipher.ptr, nil, keyptr, ivptr) {
+			return nil, errors.New("Error decrypt init")
+		}
 	}
 
 	return UnauthenticatedGCM{ctx: ctx, enc: isEncrypter}, nil
@@ -73,76 +78,6 @@ func (ugcm UnauthenticatedGCM) CryptBlocks(dst, src []byte) {
 		panic("Unexpected length difference, possible buffer overrun") // I don't think this is possible with GCM
 	}
 }
-
-// // DecryptWithoutAuthenticationGCM will decrypt a message using GCM. It will NOT authenticate the message.
-// // This is a terrible idea. Don't do this. Unless you have to, like me :(
-// func DecryptWithoutAuthenticationGCM(cipherText []byte, key []byte, iv []byte) ([]byte, error) {
-
-// 	// txtPtr := (*C.uchar)(&cipherText[0])
-// 	// txtLen := C.int(len(cipherText))
-
-// 	// result := make([]byte, txtLen+16)
-// 	// resPtr := (*C.uchar)(&result[0])
-// 	// resLen := C.int(0)
-// 	// if 1 != C.EVP_DecryptUpdate(ctx.ctx, resPtr, &resLen, txtPtr, txtLen) {
-// 	// 	return nil, errors.New("Error decrypt update")
-// 	// }
-
-// 	// return result[:resLen], nil
-
-// 	// ---------------- YESTERDAY
-
-// 	// cipher, err := GetCipherByName("aes-256-gcm")
-// 	// if err != nil {
-// 	// 	return nil, err
-// 	// }
-
-// 	// ctx, err := newCipherCtx()
-// 	// if err != nil {
-// 	// 	return nil, err
-// 	// }
-
-// 	// if 1 != C.EVP_DecryptInit_ex(ctx.ctx, cipher.ptr, nil, nil, nil) {
-// 	// 	return nil, errors.New("failed to initialize cipher context")
-// 	// }
-
-// 	// if 1 != C.EVP_CIPHER_CTX_ctrl(ctx.ctx, C.EVP_CTRL_GCM_SET_IVLEN, C.int(16), nil) {
-// 	// 	return nil, errors.New("failed to set IV length to 16")
-// 	// }
-
-// 	// if 1 != C.EVP_DecryptInit_ex(ctx.ctx, nil, nil, (*C.uchar)(&key[0]), (*C.uchar)(&iv[0])) {
-// 	// 	return nil, errors.New("failed to set key and iv for cipher context")
-// 	// }
-
-// 	// C.EVP_CIPHER_CTX_set_padding(ctx.ctx, 0)
-
-// 	// fmt.Println("BLOCKSIZE?!", cipher.BlockSize(), ctx.BlockSize())
-
-// 	// if len(cipherText) == 0 {
-// 	// 	return nil, nil
-// 	// }
-// 	// outbuf := make([]byte, len(cipherText)+16) // 16 is the block size
-// 	// outlen := C.int(len(outbuf))
-// 	// res := C.EVP_DecryptUpdate(ctx.ctx, (*C.uchar)(&outbuf[0]), &outlen,
-// 	// 	(*C.uchar)(&cipherText[0]), C.int(len(cipherText)))
-// 	// if res != 1 {
-// 	// 	return nil, fmt.Errorf("failed to decrypt [result %d]", res)
-// 	// }
-// 	// upd := outbuf[:outlen]
-
-// 	// outbuf2 := make([]byte, 16)
-// 	// var outlen2 C.int
-// 	// res = C.EVP_DecryptFinal_ex(ctx.ctx, (*C.uchar)(&outbuf2[0]), &outlen2)
-// 	// if res != 1 {
-// 	// 	// this may mean the tag failed to verify- all previous plaintext
-// 	// 	// returned must be considered faked and invalid
-// 	// 	fmt.Println("wher the fuck we at", res)
-// 	// 	return nil, errors.New("decryption failed")
-// 	// }
-
-// 	// upd = append(upd, outbuf2[:outlen2]...)
-// 	// return upd, nil
-// }
 
 const (
 	GCM_TAG_MAXLEN = 16
